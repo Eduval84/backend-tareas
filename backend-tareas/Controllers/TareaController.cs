7695fd748b50d3c4b804a4a0f90b20a261392719
evaderrama <evalderrama@domingoalonsogroup.com>
2022-01-21 23:05:02 +0000

    Agregar archivos de proyecto.

---
+++ backend-tareas/Controllers/TareaController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using backend_tareas.Context;
+using backend_tareas.Models;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_tareas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareaController : ControllerBase
+    {
+        //TODO: Revisar patron repository e implementar los interfaces y las capas necesarias 
+        private readonly AplicationDbContext _context;
+
+        public TareaController(AplicationDbContext context)
+        {
+            this._context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listTareas = await _context.Tareas.ToListAsync();
+                return Ok(listTareas);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Tarea tarea)
+        {
+            try
+            {
+                _context.Add(tarea);
+                await _context.SaveChangesAsync();
+                return Ok("Tarea creada");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] Tarea tarea)
+        {
+            try
+            {
+                _context.Remove(tarea);
+                await _context.SaveChangesAsync();
+                return Ok("Tarea eliminada");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+       
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Tarea tarea)
+        {
+            try
+            {
+                _context.Update(tarea);
+                await _context.SaveChangesAsync();
+                return Ok("Tarea actualizada");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+        }
+
+    }
+}
---
+++ backend-tareas/Startup.cs
@@ -0,0 +1,48 @@
+using backend_tareas.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace backend_tareas
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Agregamos nuestro contexto "AplicationDbContext" al contenedor de servicios y de esa manera a traves de inyeccion de dependencias vamos a poder acceder al contexto a lo largo de la aplicación
+            services.AddDbContext<AplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
